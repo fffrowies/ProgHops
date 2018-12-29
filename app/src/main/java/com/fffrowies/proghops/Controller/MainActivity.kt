9940c563247b815624d5419e029e59ecf3652ecf
fffrowies <f3rnandorowies.saxo@gmail.com>
2018-12-29 15:23:08 -0300

    Adapter as inner class in Main Activity

--- app/src/main/java/com/fffrowies/proghops/Controller/MainActivity.kt
+++ app/src/main/java/com/fffrowies/proghops/Controller/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
-import com.fffrowies.proghops.Adapter.MusiciansAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.fffrowies.proghops.Model.Musician
 import com.fffrowies.proghops.R
 import com.fffrowies.proghops.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import com.squareup.picasso.Picasso
 
-
 class MainActivity : AppCompatActivity() {
+
+    var selected = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,21 +42,50 @@ class MainActivity : AppCompatActivity() {
             layoutManager = GridLayoutManager(this@MainActivity, spanCount)
             adapter = MusiciansAdapter(musicians)
         }
+    }
 
-        val photoUrl = intent.getStringExtra("photoUrl")
-        val name = intent.getStringExtra("name")
-        //val indexPosition = intent.getStringExtra("indexPosition")
-
-        if (photoUrl != null) {
-            Picasso.get().load(photoUrl).into(imageMusicianLeft)
+    inner class MusiciansAdapter(private val musicians: ArrayList<Musician>) : RecyclerView.Adapter<MusiciansAdapter.ViewHolder>() {
+        override fun onBindViewHolder(holder: MusiciansAdapter.ViewHolder, position: Int) {
+            Picasso.get().load(musicians[position].photoUrl).into(holder.image)
+            holder.title.text = musicians[position].name
         }
-        if (name != null) {
-            textMusicianLeft.text = name
-        }
-        //if (indexPosition != null) {
-        //    musicians.removeAt(indexPosition.toInt())
-        //    recycler_view.adapter?.notifyItemRemoved(indexPosition.toInt())
-        //}
 
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusiciansAdapter.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.musician_row, parent, false)
+            val holder = ViewHolder(view)
+
+            view.setOnClickListener {
+
+                val indexPosition = holder.adapterPosition
+
+                if (selected == 0) {
+                    if (!textMusicianLeft.text.isNullOrBlank()) {
+                        
+                        println("FFFF " + textMusicianLeft.text)
+                    }
+                    textMusicianLeft.text = DataService.musician[indexPosition].name
+                    Picasso.get().load(DataService.musician[indexPosition].photoUrl).into(imageMusicianLeft)
+                    selected++
+                } else {
+                    if (!textMusicianRight.text.isNullOrBlank()) {
+                        println("FFFF " + textMusicianRight.text)
+                    }
+                    textMusicianRight.text = DataService.musician[indexPosition].name
+                    Picasso.get().load(DataService.musician[indexPosition].photoUrl).into(imageMusicianRight)
+                    selected--
+                }
+
+                musicians.removeAt(indexPosition)
+                this.notifyItemRemoved(indexPosition)
+            }
+            return holder
+        }
+
+        override fun getItemCount() = musicians.size
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val image: ImageView = itemView.findViewById(R.id.photo)
+            val title: TextView = itemView.findViewById(R.id.title)
+        }
     }
 }
